be063d37357319e442dd04ea6402920b6da1222b
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-29 15:59:35 -0600

    changes to main.jsx

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,18 +1,19 @@
-import { useEffect } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import { useLazyQuery } from '@apollo/client';
-import { QUERY_CHECKOUT } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import CartItem from '../CartItem';
-import Auth from '../../utils/auth';
-import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { loadStripes } from "@stripe/stripe-js";
+import { useLazyQuery } from "@apollo/client";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+import CartItem from "./CartItem";
+import Auth from "../../utils/auth";
+import { toggleCart, addMultipleToCart } from "../../utils/actions";
 import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
@@ -25,18 +26,14 @@ const Cart = () => {
 
   useEffect(() => {
     async function getCart() {
-      const cart = await idbPromise('cart', 'get');
-      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+      const cart = await idbPromise("cart", "get");
+      dispatch(addMultipleToCart(cart));
     }
 
     if (!state.cart.length) {
       getCart();
     }
   }, [state.cart.length, dispatch]);
-
-  function toggleCart() {
-    dispatch({ type: TOGGLE_CART });
-  }
 
   function calculateTotal() {
     let sum = 0;
@@ -62,9 +59,9 @@ const Cart = () => {
 
   if (!state.cartOpen) {
     return (
-      <div className="cart-closed" onClick={toggleCart}>
-        <span role="img" aria-label="trash">
-          🛒
+      <div className="cart-closed" onClick={() => dispatch(toggleCart())}>
+        <span role="img" aria-label='trash'>
+          🗑️
         </span>
       </div>
     );
@@ -72,7 +69,7 @@ const Cart = () => {
 
   return (
     <div className="cart">
-      <div className="close" onClick={toggleCart}>
+      <div className="close" onClick={() => dispatch(toggleCart())}>
         [close]
       </div>
       <h2>Shopping Cart</h2>
@@ -94,7 +91,7 @@ const Cart = () => {
         </div>
       ) : (
         <h3>
-          <span role="img" aria-label="shocked">
+          <span role='img' aria-label='shocked'>
             😱
           </span>
           You haven't added anything to your cart yet!
